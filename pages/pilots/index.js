d0f49e4174df1f13483c29b48c269d005eef6118
Dave <dhbriggs@yahoo.com>
2022-04-13 00:10:49 +0000

    useEffect

--- pages/pilots/index.js
+++ pages/pilots/index.js
@@ -1,22 +1,34 @@
 import Head from 'next/head'
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Pilots = () => {
-    axios.get('/.netlify/functions/getpilots')
-    .then(res => {
-      console.log(res.data);
-    })
+    const [users, setUsers] = useState()
+    useEffect(() => {
+        axios.get('/.netlify/functions/getpilots')
+            .then(res => {
+                setUsers(res.data);
+            })
+    }, [])
+
     return (
         <>
-        <Head>
-            <title>Legion Varus | pilots</title>
-            <meta name="keywords" content="pilots" />
-        </Head>
-        <div>
-            <h1>All Pilots</h1>
-        </div>
+            <Head>
+                <title>Legion Varus | pilots</title>
+                <meta name="keywords" content="pilots" />
+            </Head>
+            <div>
+                <h1>All Pilots</h1>
+                <ul>
+                    {users.map(user => (
+                        <li key={user._id}>
+                            {user.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </>
     );
 }
- 
+
 export default Pilots;
