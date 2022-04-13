9d268459dd276f8a2c9fbcc0be4449779a66c3e9
Dave <dhbriggs@yahoo.com>
2022-04-13 01:02:10 +0000

    my bad

--- netlify/functions/getpilots.js
+++ netlify/functions/getpilots.js
@@ -1,11 +1,10 @@
-var co = require('co');
 var mongoose = require('mongoose');
 
 let conn = null;
 
 const uri = process.env.MONGODB_URI;
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
 
   context.callbackWaitsForEmptyEventLoop = false;
 
@@ -17,29 +16,29 @@ exports.handler = function(event, context, callback) {
 };
 
 function run() {
-  return co(function*() {
 
-    if (conn == null) {
-      conn = yield mongoose.createConnection(uri, {
-        bufferCommands: false,
-        bufferMaxEntries: 0
-      });
-      conn.model('users', new mongoose.Schema({
-        avatar: String,
-        discriminator: String,
-        email: String,
-        ip: String,
-        name: String
-      }));
-    }
 
-    const M = conn.model('users');
+  if (conn == null) {
+    conn = yield mongoose.createConnection(uri, {
+      bufferCommands: false,
+      bufferMaxEntries: 0
+    });
+    conn.model('users', new mongoose.Schema({
+      avatar: String,
+      discriminator: String,
+      email: String,
+      ip: String,
+      name: String
+    }));
+  }
 
-    const doc = yield M.find();
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(doc)
-    };
-    return response;
-  });
+  const M = conn.model('users');
+
+  const doc = yield M.find();
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(doc)
+  };
+  return response;
+
 }
